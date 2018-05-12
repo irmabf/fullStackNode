9c9eda1463a9a7f4bcf2b7c4884acfd1bdc5d561
Irma <irblancofdz@gmail.com>
2018-05-12 20:14:59 +0200

    Add createProduct and editProduct views and routes

--- controllers/productController.js
+++ controllers/productController.js
@@ -1,4 +1,12 @@
 exports.homePage = (req, res) => {
   console.log(req.name);
   res.render('index');
+};
+
+exports.addProduct = (req, res) => {
+  res.render('editProduct', {title: 'Add Product'});
+};
+
+exports.createProduct = (req, res) => {
+  //res.json(req.body);
 }
--- routes/index.js
+++ routes/index.js
@@ -3,5 +3,7 @@ const router = express.Router();
 const productController = require('../controllers/productController');
 // Do work here
 router.get('/', productController.homePage);
+router.get('/add', productController.addProduct);
+router.post('/add', productController.createProduct);
 
 module.exports = router;
