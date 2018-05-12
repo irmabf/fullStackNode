2625ce9f5ad1d31cf42bc4c33250b50e801fffd9
Irma <irblancofdz@gmail.com>
2018-05-12 18:41:21 +0200

    Add product model

---
+++ models/Product.js
@@ -0,0 +1,28 @@
+const mongoose = require('mongoose');
+mongoose.Promise = global.Promise;
+const slug = require('slugs');
+
+const productSchema = new mongoose.Schema({
+  name: {
+    type: String, 
+    trim: true,
+    required: 'Please enter a product name!'
+  },
+  slug: String,
+  description: {
+    type: String,
+    trim: true
+  },
+  tags: [String]
+});
+
+productSchema.pre('save', function name(next) {
+  if(!this.isModified('name')){
+    next(); //skip it
+    return; //stop this function from running
+  }
+  this.slug = slug(this.name); 
+  next();
+  //TODO make more resilient so slugs are unique
+});
+module.exports = mongoose.model('Product', productSchema);
