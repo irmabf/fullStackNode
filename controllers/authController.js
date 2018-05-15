a958be15f8003e56fbed340a0e93c6831ef3c009
Irma <irblancofdz@gmail.com>
2018-05-15 16:06:27 +0200

    Try to add reset email flow, TODO: fix some issue in authController

--- controllers/authController.js
+++ controllers/authController.js
@@ -1,4 +1,6 @@
 const passport = require('passport');
+const crypto = require('crypto');
+const mongoose = require('mongoose');
 
 exports.login = passport.authenticate('local', {
   failureRedirect: '/login',
@@ -20,5 +22,23 @@ exports.isLoggedIn = (req, res, next) => {
     return;
   }
   req.flash('err', 'Oops you must be logged in to do that');
+  res.redirect('/login');
+};
+
+exports.forgot = async (req, res) => {
+  //1. See if a user with that email exists
+  const user = await User.findOne({ email: req.body.email });
+  if(!user) {
+    req.flash('error', 'No account with that email exists');
+    return res.redirect('/login');
+  }
+  //2. Set reset  tokens and expiry on their account
+  user.resetPasswordToken = crypto.randomBytes(20).toString('hex');
+  user.resetPasswordExpires = Date.now() + 3600000; //1hour from now
+  await user.save();
+  //3. Send them an email with the token
+  const resetURL = `http://${req.headers.host}.account/reset/${user.resetPasswordToken}`;
+  req.flash('success', `You have been emailed a password reset link. ${resetURL}`);
+  //4. Redirect to login page
   res.redirect('/login');
 }
