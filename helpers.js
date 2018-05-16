5f94457df02908746cdf720494193f40dd8db696
Irma <irblancofdz@gmail.com>
2018-05-16 03:07:12 +0200

    Some refactor

--- helpers.js
+++ helpers.js
@@ -21,8 +21,8 @@ exports.icon = (name) => fs.readFileSync(`./public/images/icons/${name}.svg`);
 exports.siteName = `The Reusing Net`;
 
 exports.menu = [
-  { slug: '/products', title: 'Products', icon: 'store', },
-  { slug: '/tags', title: 'Tags', icon: 'tag', },
+  { slug: '/products', title: 'All Products', icon: 'store', },
+  { slug: '/tags', title: 'Categories', icon: 'tag', },
   { slug: '/top', title: 'Top', icon: 'top', },
   { slug: '/add', title: 'Add', icon: 'add', },
   { slug: '/map', title: 'Map', icon: 'map', },
